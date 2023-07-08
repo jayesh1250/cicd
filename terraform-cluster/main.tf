261f597221c632477b9f40c9716e93137ef8af6f
Jayesh <55214904+jayesh1250@users.noreply.github.com>
2023-07-08 17:59:27 +0530

    Create main.tf

---
+++ terraform-cluster/main.tf
@@ -0,0 +1,56 @@
+data "aws_availability_zones" "azs" {}
+module "jenkins-server-vpc" {
+  source          = "terraform-aws-modules/vpc/aws"
+  name            = "jenkins-server-vpc"
+  cidr            = var.vpc_cidr_block
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
+  azs             = data.aws_availability_zones.azs.names
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+/*
+  tags = {
+    "kubernetes.io/cluster/jenkins-server-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/jenkins-server-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                  = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/jenkins-server-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"         = 1
+  }
+  */
+}
+
+
+module "eks" {
+    source  = "terraform-aws-modules/eks/aws"
+    version = "~> 19.0"
+    cluster_name = "jenkins-server-eks-cluster"
+    cluster_version = "1.24"
+
+    cluster_endpoint_public_access  = true
+
+    vpc_id = module.myjenkins-server-vpc.vpc_id
+    subnet_ids = module.myjenkins-server-vpc.private_subnets
+
+    tags = {
+        environment = "development"
+        application = "myjenkins-server"
+    }
+
+    eks_managed_node_groups = {
+        dev = {
+            min_size = 1
+            max_size = 3
+            desired_size = 2
+
+            instance_types = ["t2.small"]
+        }
+    }
+}
